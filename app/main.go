0407006f53fa2f2dee5007505bce0f69b4fb0865
itoki12 <zxxgxdfggd@gmail.com>
2022-07-03 21:16:42 +0900

    多重配列　代入

--- app/main.go
+++ app/main.go
@@ -18,36 +18,40 @@ func nextInt() int {
 	return i
 }
 
-func Min(arr []int) int {
-	min := arr[0]
-	result := 0
-	for index, value := range arr {
-		if min > value {
-			min = value
-			result = index
+func main() {
+	//sc.Split(bufio.ScanWords)
+
+	// バンドA, バンドBのライブ日を格納
+	var liveDays [31][2]int
+
+	// バンドAの入力
+	bandA := nextInt()
+	for i := 0; i < bandA; i++ {
+
+		liveDays[nextInt()-1][0] = 1
+	}
+
+	bandB := nextInt()
+	for i := 0; i < bandB; i++ {
+		liveDays[nextInt()-1][1] = 1
+
+	}
+	sameDays := 0
+	for _, day := range liveDays {
+
+		if day[0] == 1 && day[1] == 1 {
+			if sameDays%2 == 0 {
+				fmt.Println("A")
+			} else {
+				fmt.Println("B")
+			}
+			sameDays += 1
+		} else if day[0] == 1 {
+			fmt.Println("A")
+		} else if day[1] == 1 {
+			fmt.Println("B")
+		} else {
+			fmt.Println("x")
 		}
 	}
-	return result
-}
-
-func SelectionSort(arr []int) []int {
-	for i := 0; i < len(arr); i++ {
-		index := Min(arr[i:])
-		arr[i], arr[i+index] = arr[i+index], arr[i]
-	}
-	return arr
-}
-
-func main() {
-	sc.Split(bufio.ScanWords)
-	// 数式
-	length := nextInt()
-
-	var arr []int
-	for i := 0; i < length; i++ {
-		arr = append(arr, nextInt())
-	}
-
-	fmt.Println(SelectionSort(arr))
-
 }
